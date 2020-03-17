974be10a5a821bbfe59cc24ac9dc743a6c0873a7
t.akasaki <t.akasaki.csharp@gmail.com>
2020-03-17 20:24:27 +0900

    魚の実装方法を変更

--- src/main/java/jp/artan/japanesefoodmod/raw_materials/JapaneseFoodRawMaterials.java
+++ src/main/java/jp/artan/japanesefoodmod/raw_materials/JapaneseFoodRawMaterials.java
@@ -3,21 +3,28 @@ package jp.artan.japanesefoodmod.raw_materials;
 import jp.artan.japanesefoodmod.common.crop.SameCropAsSeed;
 import jp.artan.japanesefoodmod.common.food.JapaneseFood;
 import jp.artan.japanesefoodmod.raw_materials.crop.RawMaterialSameCropAsSeed;
-import jp.artan.japanesefoodmod.raw_materials.fish.RawMaterialFish;
 import jp.artan.japanesefoodmod.raw_materials.food.RawMaterialFood;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class JapaneseFoodRawMaterials {
+    // クリエイティブタブ
     public static CreativeTabs creativeTab = new RawMaterialsCreativeTab();
 
+    // 果樹
     public static final JapaneseFood Banana = new RawMaterialFood("banana", 1, 0.5F);
 
+    // 作物
     public static final SameCropAsSeed Soy = new RawMaterialSameCropAsSeed("soy", 1, 0.5F);
 
-    public static final RawMaterialFish FISH = new RawMaterialFish("fish");
+    // 魚
+    public static final JapaneseFood FISH = new RawMaterialFood("fish", 1, 0.5F);
+    public static final JapaneseFood FISH_BAKED = new RawMaterialFood("fish_baked", 2, 1F);
+    public static final JapaneseFood FISH2 = new RawMaterialFood("fish2", 1, 0.5F);
 
     public static void preInit(FMLPreInitializationEvent event) {
     }
@@ -28,7 +35,7 @@ public class JapaneseFoodRawMaterials {
      * @param event
      */
     public static void init(FMLInitializationEvent event) {
-        FISH.registerSmelting();
+        GameRegistry.addSmelting(FISH, new ItemStack(FISH_BAKED, 1), 0.4F);
     }
 
     public static void postInit(FMLPostInitializationEvent event) {
