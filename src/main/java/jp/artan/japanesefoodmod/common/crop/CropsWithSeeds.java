3f1653aaab6194e16b5f02f3ca7051e148e7c02a
t.akasaki <t.akasaki.csharp@gmail.com>
2020-03-17 20:45:02 +0900

    コメント追加

--- src/main/java/jp/artan/japanesefoodmod/common/crop/CropsWithSeeds.java
+++ src/main/java/jp/artan/japanesefoodmod/common/crop/CropsWithSeeds.java
@@ -4,6 +4,30 @@ import jp.artan.japanesefoodmod.common.food.JapaneseFood;
 import jp.artan.japanesefoodmod.common.food.JapaneseFoodPlant;
 import jp.artan.japanesefoodmod.common.food.JapaneseFoodSeed;
 
+/**
+ * 種と作物が異なる作物を作成するクラス
+ * @apiNote
+ * ./blockstates/${name}.json
+ *      成長段階に合わせたブロックの情報を記述（０～７の８段階）
+ * ./lang/en_us.lang　言語設定
+ *      item.${name}.name=XXXXX
+ *      item.${name}_seeds.name=XXXXX
+ * ./lang/ja_JP.lang　言語設定
+ *      item.${name}.name=XXXXX
+ *      item.${name}_seeds.name=XXXXX
+ * ./models/block/${name}_plant${Age(0~7)}.json
+ *      成長段階に合わせたブロックのテクスチャ情報を記述
+ * ./models/item/${name}_plant.json
+ *      ブロックの情報を記述
+ * ./models/item/${name}.json
+ * ./models/item/${name}_seeds.json
+ *      アイテムのテクスチャ情報を記述
+ * ./textures/block/${name}_plant${Age(0~7)}.png
+ *      ブロックの情報を記述
+ * ./textures/item/${name}.png
+ * ./textures/item/${name}_seeds.png
+ *      アイテムのテクスチャ情報を記述
+ */
 public abstract class CropsWithSeeds {
 
     public final JapaneseFoodPlant Plant;
--- src/main/java/jp/artan/japanesefoodmod/common/crop/SameCropAsSeed.java
+++ src/main/java/jp/artan/japanesefoodmod/common/crop/SameCropAsSeed.java
@@ -3,6 +3,26 @@ package jp.artan.japanesefoodmod.common.crop;
 import jp.artan.japanesefoodmod.common.food.JapaneseFoodAsSeed;
 import jp.artan.japanesefoodmod.common.food.JapaneseFoodPlant;
 
+/**
+ * 種と作物が同じ作物を作成するクラス
+ * @apiNote
+ * ./blockstates/${name}.json
+ *      成長段階に合わせたブロックの情報を記述（０～７の８段階）
+ * ./lang/en_us.lang　言語設定
+ *      item.${name}_seeds.name=XXXXX
+ * ./lang/ja_JP.lang　言語設定
+ *      item.${name}_seeds.name=XXXXX
+ * ./models/block/${name}_plant${Age(0~7)}.json
+ *      成長段階に合わせたブロックのテクスチャ情報を記述
+ * ./models/item/${name}_plant.json
+ *      ブロックの情報を記述
+ * ./models/item/${name}_seeds.json
+ *      アイテムのテクスチャ情報を記述
+ * ./textures/block/${name}_plant${Age(0~7)}.png
+ *      ブロックの情報を記述
+ * ./textures/item/${name}_seeds.png
+ *      アイテムのテクスチャ情報を記述
+ */
 public abstract class SameCropAsSeed {
 
     public final JapaneseFoodPlant Plant;
