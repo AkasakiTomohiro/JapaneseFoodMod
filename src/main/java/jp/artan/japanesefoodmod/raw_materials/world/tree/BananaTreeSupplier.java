19a6bf68c7caa1bde1d10e27aae278b07d8d359c
t.akasaki <t.akasaki.csharp@gmail.com>
2020-03-18 22:50:10 +0900

    果樹ブロック（現在は土）を配置するロジックを追加

--- src/main/java/jp/artan/japanesefoodmod/raw_materials/world/tree/BananaTreeSupplier.java
+++ src/main/java/jp/artan/japanesefoodmod/raw_materials/world/tree/BananaTreeSupplier.java
@@ -1,5 +1,6 @@
 package jp.artan.japanesefoodmod.raw_materials.world.tree;
 
+import jp.artan.japanesefoodmod.JapaneseFoodMod;
 import jp.artan.japanesefoodmod.common.wood.FruitLeaves;
 import jp.artan.japanesefoodmod.common.wood.FruitLog;
 import jp.artan.japanesefoodmod.common.world.WorldGenFruitTrees;
@@ -31,71 +32,62 @@ public class BananaTreeSupplier extends WorldGenSupplier {
 }
 
 class BananaTreeGen extends WorldGenFruitTrees {
-    private final int minTreeHeight = 4;
+    private final int minTreeHeight = 5;
 
     public BananaTreeGen(boolean parShouldNotify, FruitLog log, FruitLeaves leaves) {
         super(parShouldNotify, log, leaves);
     }
 
     @Override
-    public boolean generate(World parWorld, Random parRandom, BlockPos parBlockPos)
-    {
+    public boolean generate(World parWorld, Random parRandom, BlockPos parBlockPos) {
         int minHeight = parRandom.nextInt(3) + minTreeHeight;
 
         // Check if tree fits in world
-        if (parBlockPos.getY() >= 1 && parBlockPos.getY() + minHeight + 1 <= parWorld.getHeight())
-        {
-            if (!isSuitableLocation(parWorld, parBlockPos, minHeight))
-            {
+        if (parBlockPos.getY() >= 1 && parBlockPos.getY() + minHeight + 1 <= parWorld.getHeight()) {
+            if (!isSuitableLocation(parWorld, parBlockPos, minHeight)) {
                 return false;
-            }
-            else
-            {
+            } else {
                 IBlockState state = parWorld.getBlockState(parBlockPos.down());
 
-                if (state.getBlock().canSustainPlant(state, parWorld, parBlockPos.down(), EnumFacing.UP, (IPlantable) Blocks.SAPLING) && parBlockPos.getY() < parWorld.getHeight() - minHeight - 1)
-                {
+                if (state.getBlock().canSustainPlant(state, parWorld, parBlockPos.down(), EnumFacing.UP,
+                        (IPlantable) Blocks.SAPLING) && parBlockPos.getY() < parWorld.getHeight() - minHeight - 1) {
                     state.getBlock().onPlantGrow(state, parWorld, parBlockPos.down(), parBlockPos);
                     generateLeaves(parWorld, parBlockPos, minHeight, parRandom);
                     generateTrunk(parWorld, parBlockPos, minHeight);
+                    generateFlits(parWorld, parBlockPos, minHeight, parRandom);
                     return true;
-                }
-                else
-                {
+                } else {
                     return false;
                 }
             }
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
 
-    private void generateLeaves(World parWorld, BlockPos parBlockPos, int height, Random parRandom)
-    {
-        for (int foliageY = parBlockPos.getY() - 3 + height; foliageY <= parBlockPos.getY() + height; ++foliageY)
-        {
+    private void generateLeaves(World parWorld, BlockPos parBlockPos, int height, Random parRandom) {
+        for (int foliageY = parBlockPos.getY() - 3 + height; foliageY <= parBlockPos.getY() + height; ++foliageY) {
             int foliageLayer = foliageY - (parBlockPos.getY() + height);
             int foliageLayerRadius = 1 - foliageLayer / 2;
 
-            for (int foliageX = parBlockPos.getX() - foliageLayerRadius; foliageX <= parBlockPos.getX() + foliageLayerRadius; ++foliageX)
-            {
+            for (int foliageX = parBlockPos.getX() - foliageLayerRadius; foliageX <= parBlockPos.getX()
+                    + foliageLayerRadius; ++foliageX) {
                 int foliageRelativeX = foliageX - parBlockPos.getX();
 
-                for (int foliageZ = parBlockPos.getZ() - foliageLayerRadius; foliageZ <= parBlockPos.getZ() + foliageLayerRadius; ++foliageZ)
-                {
+                for (int foliageZ = parBlockPos.getZ() - foliageLayerRadius; foliageZ <= parBlockPos.getZ()
+                        + foliageLayerRadius; ++foliageZ) {
                     int foliageRelativeZ = foliageZ - parBlockPos.getZ();
 
                     // Fill in layer with some randomness
-                    if (Math.abs(foliageRelativeX) != foliageLayerRadius || Math.abs(foliageRelativeZ) != foliageLayerRadius || parRandom.nextInt(2) != 0 && foliageLayer != 0)
-                    {
-                        BlockPos blockPos = new BlockPos(foliageX, foliageY, foliageZ);
-                        IBlockState state = parWorld.getBlockState(blockPos);
+                    if (Math.abs(foliageRelativeX) != foliageLayerRadius
+                            || Math.abs(foliageRelativeZ) != foliageLayerRadius
+                            || parRandom.nextInt(2) != 0 && foliageLayer != 0) {
+                        BlockPos blockPosLeaves = new BlockPos(foliageX, foliageY, foliageZ);
+                        IBlockState stateLeaves = parWorld.getBlockState(blockPosLeaves);
 
-                        if (state.getBlock().isAir(state, parWorld, blockPos) || state.getBlock().isLeaves(state, parWorld, blockPos))
-                        {
-                            setBlockAndNotifyAdequately(parWorld, blockPos, blockStateLeaves);
+                        if (stateLeaves.getBlock().isAir(stateLeaves, parWorld, blockPosLeaves)
+                                || stateLeaves.getBlock().isLeaves(stateLeaves, parWorld, blockPosLeaves)) {
+                            setBlockAndNotifyAdequately(parWorld, blockPosLeaves, blockStateLeaves);
                         }
                     }
                 }
@@ -103,46 +95,66 @@ class BananaTreeGen extends WorldGenFruitTrees {
         }
     }
 
-    private void generateTrunk(World parWorld, BlockPos parBlockPos, int minHeight)
-    {
-        for (int height = 0; height < minHeight; ++height)
-        {
-            BlockPos upN = parBlockPos.up(height);
-            IBlockState state = parWorld.getBlockState(upN);
+    private void generateFlits(World parWorld, BlockPos parBlockPos, int height, Random parRandom) {
+        int foliageY = parBlockPos.getY() - 3 + height - 1;
+        int foliageLayer = foliageY - (parBlockPos.getY() + height);
+        int foliageLayerRadius = 1 - foliageLayer / 2;
 
-            if (state.getBlock().isAir(state, parWorld, upN) || state.getBlock().isLeaves(state, parWorld, upN))
-            {
-                setBlockAndNotifyAdequately(parWorld, parBlockPos.up(height), blockStateWood.withProperty(BlockLog.LOG_AXIS, BlockLog.EnumAxis.Y));
+        for (int foliageX = parBlockPos.getX() - foliageLayerRadius; foliageX <= parBlockPos.getX()
+                + foliageLayerRadius; ++foliageX) {
+
+            for (int foliageZ = parBlockPos.getZ() - foliageLayerRadius; foliageZ <= parBlockPos.getZ()
+                    + foliageLayerRadius; ++foliageZ) {
+
+                BlockPos blockPos = new BlockPos(foliageX, foliageY, foliageZ);
+                IBlockState state = parWorld.getBlockState(blockPos);
+
+                IBlockState stateLeaves = parWorld.getBlockState(new BlockPos(foliageX, foliageY + 1, foliageZ));
+
+                if (state.getBlock().isAir(state, parWorld, blockPos)
+                        && state.getBlock().isLeaves(stateLeaves, parWorld, blockPos) && parRandom.nextInt(10) == 1) {
+                    JapaneseFoodMod.logger
+                            .info("foliageX: " + foliageX + ", foliageY: " + foliageY + ", foliageZ: " + foliageZ);
+                    setBlockAndNotifyAdequately(parWorld, blockPos, Blocks.DIRT.getDefaultState());
+                }
             }
         }
     }
 
-    private boolean isSuitableLocation(World parWorld, BlockPos parBlockPos, int minHeight)
-    {
+    private void generateTrunk(World parWorld, BlockPos parBlockPos, int minHeight) {
+        for (int height = 0; height < minHeight; ++height) {
+            BlockPos upN = parBlockPos.up(height);
+            IBlockState state = parWorld.getBlockState(upN);
+
+            if (state.getBlock().isAir(state, parWorld, upN) || state.getBlock().isLeaves(state, parWorld, upN)) {
+                setBlockAndNotifyAdequately(parWorld, parBlockPos.up(height),
+                        blockStateWood.withProperty(BlockLog.LOG_AXIS, BlockLog.EnumAxis.Y));
+            }
+        }
+    }
+
+    private boolean isSuitableLocation(World parWorld, BlockPos parBlockPos, int minHeight) {
         boolean isSuitableLocation = true;
 
-        for (int checkY = parBlockPos.getY(); checkY <= parBlockPos.getY() + 1 + minHeight; ++checkY)
-        {
+        for (int checkY = parBlockPos.getY(); checkY <= parBlockPos.getY() + 1 + minHeight; ++checkY) {
             // Handle increasing space towards top of tree
             int extraSpaceNeeded = 1;
             // Handle base location
-            if (checkY == parBlockPos.getY())
-            {
+            if (checkY == parBlockPos.getY()) {
                 extraSpaceNeeded = 0;
             }
             // Handle top location
-            if (checkY >= parBlockPos.getY() + 1 + minHeight - 2)
-            {
+            if (checkY >= parBlockPos.getY() + 1 + minHeight - 2) {
                 extraSpaceNeeded = 2;
             }
 
             BlockPos.MutableBlockPos blockPos = new BlockPos.MutableBlockPos();
 
-            for (int checkX = parBlockPos.getX() - extraSpaceNeeded; checkX <= parBlockPos.getX() + extraSpaceNeeded && isSuitableLocation; ++checkX)
-            {
-                for (int checkZ = parBlockPos.getZ() - extraSpaceNeeded; checkZ <= parBlockPos.getZ() + extraSpaceNeeded && isSuitableLocation; ++checkZ)
-                {
-                    isSuitableLocation = this.isReplaceable(parWorld,blockPos.setPos(checkX, checkY, checkZ));
+            for (int checkX = parBlockPos.getX() - extraSpaceNeeded; checkX <= parBlockPos.getX() + extraSpaceNeeded
+                    && isSuitableLocation; ++checkX) {
+                for (int checkZ = parBlockPos.getZ() - extraSpaceNeeded; checkZ <= parBlockPos.getZ() + extraSpaceNeeded
+                        && isSuitableLocation; ++checkZ) {
+                    isSuitableLocation = this.isReplaceable(parWorld, blockPos.setPos(checkX, checkY, checkZ));
                 }
             }
         }
