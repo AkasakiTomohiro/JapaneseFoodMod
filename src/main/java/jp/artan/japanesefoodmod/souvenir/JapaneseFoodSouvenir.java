f77a09b54ad0078f7a6c1d8d26cad1c67b114e9f
t.akasaki <t.akasaki.csharp@gmail.com>
2020-03-24 00:07:24 +0900

    新潟と富山のお土産を追加

--- src/main/java/jp/artan/japanesefoodmod/souvenir/JapaneseFoodSouvenir.java
+++ src/main/java/jp/artan/japanesefoodmod/souvenir/JapaneseFoodSouvenir.java
@@ -23,8 +23,10 @@ public class JapaneseFoodSouvenir {
     public static final JapaneseFood TABIGARASU = new SouvenirFood("tabigarasu", 1, 0.2F);
     public static final JapaneseFood PEANUTS_MONAKA = new SouvenirFood("peanuts_monaka", 1, 1.0F);
     public static final JapaneseFood SOUKA_SENBEI = new SouvenirFood("souka_senbei", 2, 1.0F);
-    public static final JapaneseFood HATO_SABLE = new SouvenirFood("hato_sable", 2, 1.0F);
     public static final JapaneseFood TokyoBanana = new SouvenirFood("tokyo_banana", 2, 0.5F);
+    public static final JapaneseFood HATO_SABLE = new SouvenirFood("hato_sable", 2, 1.0F);
+    public static final JapaneseFood SASADANGO = new SouvenirFood("sasadango", 2, 1.2F);
+    public static final JapaneseFood KANOKOMOTI = new SouvenirFood("kanokomoti", 1, 0.6F);
 
     public static void registerModels(ModelRegistryEvent event) {
     }
