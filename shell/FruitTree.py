a5094dcab2c24b1836b104c1f613977344c47ea3
t.akasaki <t.akasaki.csharp@gmail.com>
2020-05-13 09:05:33 +0900

    関数の調整

--- shell/FruitTree.py
+++ shell/FruitTree.py
@@ -8,35 +8,32 @@ parser.add_argument('files', metavar='S', type=str, nargs='+')
 
 jsonFileBaseDir = os.getcwd() + r"\src\main\resources\assets\japanesefoodmod"
 
+def create_json(fileName, data):
+    with open(fileName, 'w') as f:
+        json.dump(data, f, indent = 2)
+    print("Create File: " , fileName)
+
 def create_block_states_fruit_block(name):
     fileName = jsonFileBaseDir + "\\blockstates\\" + name + "_fruit_block.json"
     data = {"variants": {"inventory": [{ "model": "japanesefoodmod:fruits/" + name + "_fruit_block_0"}]}}
     for i in range(3):
         data["variants"]["age=" + str(i)] = {"model": "japanesefoodmod:fruits/" + name + "_fruit_block_" + str(i)}
-    with open(fileName, 'w') as f:
-        json.dump(data, f, indent = 2)
-    print("Create File: " , fileName)
+    create_json(fileName, data)
 
 def create_block_states_leaves(name):
     fileName = jsonFileBaseDir + "\\blockstates\\" + name + "_leaves.json"
     data = {"forge_marker":1,"defaults":{"model":"japanesefoodmod:trees/" + name + "_leaves","uvlock":True},"variants":{"normal":[{}],"inventory":[{}],"check_decay=true,decayable=true":[{}],"check_decay=true,decayable=false":[{}],"check_decay=false,decayable=true":[{}],"check_decay=false,decayable=false":[{}]}}
-    with open(fileName, 'w') as f:
-        json.dump(data, f, indent = 2)
-    print("Create File: " , fileName)
+    create_json(fileName, data)
 
 def create_block_states_log(name):
     fileName = jsonFileBaseDir + "\\blockstates\\" + name + "_log.json"
     data = {"forge_marker":1,"variants":{"axis=y":{"model":"japanesefoodmod:trees/" + name + "_log"},"axis=z":{"model":"japanesefoodmod:trees/" + name + "_log","x":90},"axis=x":{"model":"japanesefoodmod:trees/" + name + "_log","x":90,"y":90},"axis=none":{"model":"japanesefoodmod:trees/" + name + "_log"}}}
-    with open(fileName, 'w') as f:
-        json.dump(data, f, indent = 2)
-    print("Create File: " , fileName)
+    create_json(fileName, data)
 
 def create_block_states_sapling(name):
     fileName = jsonFileBaseDir + "\\blockstates\\" + name + "_sapling.json"
     data = {"forge_marker":1,"defaults":{"model":"japanesefoodmod:trees/" + name + "_sapling","uvlock":True},"variants":{"normal":[{}],"inventory":[{}],"stage=0":[{}],"stage=1":[{}]}}
-    with open(fileName, 'w') as f:
-        json.dump(data, f, indent = 2)
-    print("Create File: " , fileName)
+    create_json(fileName, data)
 
 def create_block_states(name):
     create_block_states_fruit_block(name)
@@ -45,40 +42,30 @@ def create_block_states(name):
     create_block_states_sapling(name)
 
 def create_models_item(name):
-    itemSeedsFileName = jsonFileBaseDir + "\\models\\item\\" + name + ".json"
-    seedsData = {"parent": "item/generated", "textures": {"layer0": "japanesefoodmod:items/" + name}}
-    with open(itemSeedsFileName, 'w') as f:
-        json.dump(seedsData, f, indent = 2)
-    print("Create File: " , itemSeedsFileName)
+    fileName = jsonFileBaseDir + "\\models\\item\\" + name + ".json"
+    data = {"parent": "item/generated", "textures": {"layer0": "japanesefoodmod:items/" + name}}
+    create_json(fileName, data)
 
 def create_models_block_leaves(name):
-    itemSeedsFileName = jsonFileBaseDir + "\\models\\block\\trees\\" + name + "_leaves.json"
-    seedsData = {"parent":"block/leaves","textures":{"all":"japanesefoodmod:blocks/trees/" + name + "_leaves"}}
-    with open(itemSeedsFileName, 'w') as f:
-        json.dump(seedsData, f, indent = 2)
-    print("Create File: " , itemSeedsFileName)
+    fileName = jsonFileBaseDir + "\\models\\block\\trees\\" + name + "_leaves.json"
+    data = {"parent":"block/leaves","textures":{"all":"japanesefoodmod:blocks/trees/" + name + "_leaves"}}
+    create_json(fileName, data)
 
 def create_models_block_log(name):
-    itemSeedsFileName = jsonFileBaseDir + "\\models\\block\\trees\\" + name + "_log.json"
-    seedsData = {"parent":"block/cube_column","textures":{"end":"japanesefoodmod:blocks/trees/" + name + "_log_top","side":"japanesefoodmod:blocks/trees/" + name + "_log_side"}}
-    with open(itemSeedsFileName, 'w') as f:
-        json.dump(seedsData, f, indent = 2)
-    print("Create File: " , itemSeedsFileName)
+    fileName = jsonFileBaseDir + "\\models\\block\\trees\\" + name + "_log.json"
+    data = {"parent":"block/cube_column","textures":{"end":"japanesefoodmod:blocks/trees/" + name + "_log_top","side":"japanesefoodmod:blocks/trees/" + name + "_log_side"}}
+    create_json(fileName, data)
 
 def create_models_block_sapling(name):
-    itemSeedsFileName = jsonFileBaseDir + "\\models\\block\\trees\\" + name + "_sapling.json"
-    seedsData = {"parent":"block/cross","textures":{"cross":"japanesefoodmod:blocks/trees/" + name + "_sapling"}}
-    with open(itemSeedsFileName, 'w') as f:
-        json.dump(seedsData, f, indent = 2)
-    print("Create File: " , itemSeedsFileName)
+    fileName = jsonFileBaseDir + "\\models\\block\\trees\\" + name + "_sapling.json"
+    data = {"parent":"block/cross","textures":{"cross":"japanesefoodmod:blocks/trees/" + name + "_sapling"}}
+    create_json(fileName, data)
 
 def create_models_block_fruits(name):
     for i in range(3):
-        itemPlantFileName = jsonFileBaseDir + "\\models\\block\\fruits\\" + name + "_fruit_block_" + str(i) + ".json"
-        plantData = {"parent":"block/cross","textures": {"cross": "japanesefoodmod:blocks/fruits/" + name + "_fruit_block_" + str(i)}}
-        with open(itemPlantFileName, 'w') as f:
-            json.dump(plantData, f, indent = 2)
-        print("Create File: " , itemPlantFileName)
+        fileName = jsonFileBaseDir + "\\models\\block\\fruits\\" + name + "_fruit_block_" + str(i) + ".json"
+        data = {"parent":"block/cross","textures": {"cross": "japanesefoodmod:blocks/fruits/" + name + "_fruit_block_" + str(i)}}
+        create_json(fileName, data)
 
 def create_models_block(name):
     create_models_block_leaves(name)
@@ -87,32 +74,24 @@ def create_models_block(name):
     create_models_block_fruits(name)
 
 def create_models_item_fruits(name):
-    itemSeedsFileName = jsonFileBaseDir + "\\models\\item\\" + name + ".json"
-    seedsData = {"parent":"item/generated","textures":{"layer0":"japanesefoodmod:items/" + name}}
-    with open(itemSeedsFileName, 'w') as f:
-        json.dump(seedsData, f, indent = 2)
-    print("Create File: " , itemSeedsFileName)
+    fileName = jsonFileBaseDir + "\\models\\item\\" + name + ".json"
+    data = {"parent":"item/generated","textures":{"layer0":"japanesefoodmod:items/" + name}}
+    create_json(fileName, data)
 
 def create_models_item_leaves(name):
-    itemSeedsFileName = jsonFileBaseDir + "\\models\\item\\" + name + "_leaves.json"
-    seedsData = {"parent":"japanesefoodmod:block/" + name + "_leaves"}
-    with open(itemSeedsFileName, 'w') as f:
-        json.dump(seedsData, f, indent = 2)
-    print("Create File: " , itemSeedsFileName)
+    fileName = jsonFileBaseDir + "\\models\\item\\" + name + "_leaves.json"
+    data = {"parent":"japanesefoodmod:block/" + name + "_leaves"}
+    create_json(fileName, data)
 
 def create_models_item_log(name):
-    itemSeedsFileName = jsonFileBaseDir + "\\models\\item\\" + name + "_log.json"
-    seedsData = {"parent":"japanesefoodmod:block/trees/" + name + "_log"}
-    with open(itemSeedsFileName, 'w') as f:
-        json.dump(seedsData, f, indent = 2)
-    print("Create File: " , itemSeedsFileName)
+    fileName = jsonFileBaseDir + "\\models\\item\\" + name + "_log.json"
+    data = {"parent":"japanesefoodmod:block/trees/" + name + "_log"}
+    create_json(fileName, data)
 
 def create_models_item_sapling(name):
-    itemSeedsFileName = jsonFileBaseDir + "\\models\\item\\" + name + "_sapling.json"
-    seedsData = {"parent":"item/generated","textures":{"layer0":"japanesefoodmod:blocks/" + name + "_sapling"}}
-    with open(itemSeedsFileName, 'w') as f:
-        json.dump(seedsData, f, indent = 2)
-    print("Create File: " , itemSeedsFileName)
+    fileName = jsonFileBaseDir + "\\models\\item\\" + name + "_sapling.json"
+    data = {"parent":"item/generated","textures":{"layer0":"japanesefoodmod:blocks/" + name + "_sapling"}}
+    create_json(fileName, data)
 
 def create_models_item(name):
     create_models_item_fruits(name)
--- shell/SameCropAsSeed.py
+++ shell/SameCropAsSeed.py
@@ -8,36 +8,33 @@ parser.add_argument('files', metavar='S', type=str, nargs='+')
 
 jsonFileBaseDir = os.getcwd() + r"\src\main\resources\assets\japanesefoodmod"
 
+def create_json(fileName, data):
+    with open(fileName, 'w') as f:
+        json.dump(data, f, indent = 2)
+    print("Create File: " , fileName)
+
 def create_block_states(name):
     fileName = jsonFileBaseDir + "\\blockstates\\" + name + "_plant.json"
     data = {"variants": {}}
     for i in range(8):
         data["variants"]["age=" + str(i)] = {"model": "japanesefoodmod:plants/" + name + "_plant_" + str(i)}
-    with open(fileName, 'w') as f:
-        json.dump(data, f, indent = 2)
-    print("Create File: " , fileName)
+    create_json(fileName, data)
 
 def create_models_item_seed(name):
-    itemSeedsFileName = jsonFileBaseDir + "\\models\\item\\" + name + "_seeds.json"
-    seedsData = {"parent": "item/generated", "textures": {"layer0": "japanesefoodmod:items/" + name + "_seeds"}}
-    with open(itemSeedsFileName, 'w') as f:
-        json.dump(seedsData, f, indent = 2)
-    print("Create File: " , itemSeedsFileName)
+    fileName = jsonFileBaseDir + "\\models\\item\\" + name + "_seeds.json"
+    data = {"parent": "item/generated", "textures": {"layer0": "japanesefoodmod:items/" + name + "_seeds"}}
+    create_json(fileName, data)
 
 def create_models_item_plant(name):
-    itemPlantFileName = jsonFileBaseDir + "\\models\\item\\" + name + "_plant.json"
-    plantData = {"parent": "block/cube_all", "textures": {"all": "japanesefoodmod:blocks/" + name + "_plant" }}
-    with open(itemPlantFileName, 'w') as f:
-        json.dump(plantData, f, indent = 2)
-    print("Create File: " , itemPlantFileName)
+    fileName = jsonFileBaseDir + "\\models\\item\\" + name + "_plant.json"
+    data = {"parent": "block/cube_all", "textures": {"all": "japanesefoodmod:blocks/" + name + "_plant" }}
+    create_json(fileName, data)
 
 def create_models_block(name):
     for i in range(8):
-        itemPlantFileName = jsonFileBaseDir + "\\models\\block\\plants\\" + name + "_plant_" + str(i) + ".json"
-        plantData = {"parent": "block/crop", "textures": {"crop": "japanesefoodmod:blocks/plants/" + name + "_plant_" + str(i) }}
-        with open(itemPlantFileName, 'w') as f:
-            json.dump(plantData, f, indent = 2)
-        print("Create File: " , itemPlantFileName)
+        fileName = jsonFileBaseDir + "\\models\\block\\plants\\" + name + "_plant_" + str(i) + ".json"
+        data = {"parent": "block/crop", "textures": {"crop": "japanesefoodmod:blocks/plants/" + name + "_plant_" + str(i) }}
+        create_json(fileName, data)
 
 def pascal_conversion(s):
     return s.capitalize()
