e8b4c8bf9b9fd5ee43dc96cf28a3e6733252d113
t.akasaki <t.akasaki.csharp@gmail.com>
2020-05-13 09:18:39 +0900

    出力結果を50個ずつに分割する

--- shell/SpreadSheet.py
+++ shell/SpreadSheet.py
@@ -3,6 +3,10 @@ import csv
 csv_file = open("./shell/JapaneaseFoodMod - 実装アイテム一覧.csv", "r", encoding="utf-8", errors="", newline="" )
 f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 
+def split_list(l, n):
+    for idx in range(0, len(l), n):
+        yield l[idx:idx + n]
+
 Food = []
 CropsWithSeeds = []
 FruitTree = []
@@ -17,12 +21,20 @@ for row in f:
         FruitTree.append(row[5])
     if(row[1] == "SameCropAsSeed" and row[5] != ""):
         SameCropAsSeed.append(row[5])
-
-print("\nFood")
-print("python shell/Food.py " + " ".join(Food))
-print("\nCropsWithSeeds")
-print("python shell/CropsWithSeeds.py " + " ".join(CropsWithSeeds))
-print("\nFruitTree")
-print("python shell/FruitTree.py " + " ".join(FruitTree))
-print("\nSameCropAsSeed")
-print("python shell/SameCropAsSeed.py " + " ".join(SameCropAsSeed))
+        
+print("\nFood: " + str(len(Food)))
+Food = split_list(Food, 50)
+for food in Food:
+    print("python shell/Food.py " + " ".join(food))
+print("\nCropsWithSeeds: " + str(len(CropsWithSeeds)))
+CropsWithSeeds = split_list(CropsWithSeeds, 50)
+for cropsWithSeeds in CropsWithSeeds:
+    print("python shell/CropsWithSeeds.py " + " ".join(cropsWithSeeds))
+print("\nFruitTree: " + str(len(FruitTree)))
+FruitTree = split_list(FruitTree, 50)
+for fruitTree in FruitTree:
+    print("python shell/FruitTree.py " + " ".join(fruitTree))
+print("\nSameCropAsSeed: " + str(len(SameCropAsSeed)))
+SameCropAsSeed = split_list(SameCropAsSeed, 50)
+for sameCropAsSeed in SameCropAsSeed:
+    print("python shell/SameCropAsSeed.py " + " ".join(sameCropAsSeed))
